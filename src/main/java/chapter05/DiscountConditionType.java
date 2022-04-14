0e7a35c7dcfa3e75d7339507cae501dee548cc1f
who-hoo <who.ho3ov@gmail.com>
2022-04-15 01:57:15 +0900

    :recycle: DiscountCondition을 SequenceCondition과 PeriodCondition 두 개의 타입으로 분리
    :poop: 분리한 후에 전체적인 결합도가 오히려 높아짐(DiscountCondition 하나의 클래스에만 결합돼 있었는데 분리한 후에 분리된 클래스 양쪽 모두에게 결합)
    :poop: 분리한 후에 새로운 할인 조건을 추가하기 더 어려워짐(할인 조건을 추가하려면 Movie도 함께 수정해야 함)

--- src/main/java/chapter05/DiscountConditionType.java
+++
@@ -1,6 +0,0 @@
-package chapter05;
-
-public enum DiscountConditionType {
-    SEQUENCE,       // 순번조건
-    PERIOD          // 기간 조건
-}
---
+++ src/main/java/chapter05/SequenceCondition.java
@@ -0,0 +1,14 @@
+package chapter05;
+
+public class SequenceCondition {
+
+    private int sequence;
+
+    public SequenceCondition(int sequence) {
+        this.sequence = sequence;
+    }
+
+    public boolean isSatisfiedBy(Screening screening) {
+        return sequence == screening.getSequence();
+    }
+}
