7935ce1ca07bb428f9d84a288b9da6e8a16cbd22
who-hoo <who.ho3ov@gmail.com>
2022-04-15 01:35:57 +0900

    :sparkles: 책임주도설계 방식으로 영화예매시스템 구현

---
+++ src/main/java/chapter05/Reservation.java
@@ -0,0 +1,19 @@
+package chapter05;
+
+import chapter02.Customer;
+import chapter02.Money;
+
+public class Reservation {
+
+    private Customer customer;
+    private Screening Screening;
+    private Money fee;
+    private int audienceCount;
+
+    public Reservation(Customer customer, Screening Screening, Money fee, int audienceCount) {
+        this.customer = customer;
+        this.Screening = Screening;
+        this.fee = fee;
+        this.audienceCount = audienceCount;
+    }
+}
---
+++ src/main/java/chapter05/Screening.java
@@ -0,0 +1,28 @@
+package chapter05;
+
+import chapter02.Customer;
+import chapter02.Money;
+import java.time.LocalDateTime;
+
+public class Screening {
+
+    private Movie movie;
+    private int sequence;
+    private LocalDateTime whenScreened;
+
+    public Reservation reserve(Customer customer, int audienceCount) {
+        return new Reservation(customer, this, calculateFee(audienceCount), audienceCount);
+    }
+
+    private Money calculateFee(int audienceCount) {
+        return movie.calculateMovieFee(this).times(audienceCount);
+    }
+
+    public LocalDateTime getWhenScreened() {
+        return whenScreened;
+    }
+
+    public int getSequence() {
+        return sequence;
+    }
+}
