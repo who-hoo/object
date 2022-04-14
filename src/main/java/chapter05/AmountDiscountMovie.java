275da14bfc4bedbbecb4f5baecc6e3d88a5daa94
who-hoo <who.ho3ov@gmail.com>
2022-04-15 02:45:42 +0900

    :recycle: Movie 클래스 개선
    - 금액 할인 정책 영화와 비율 할인 정책 영화를 분리
    - 구현을 공유할 필요가 있기 때문에 추상 클래스를 이용하여 역할을 구현

---
+++ src/main/java/chapter05/AmountDiscountMovie.java
@@ -0,0 +1,19 @@
+package chapter05;
+
+import chapter02.Money;
+import java.time.Duration;
+
+public class AmountDiscountMovie extends Movie {
+
+    private Money discountAmount;
+
+    public AmountDiscountMovie(String title, Duration runningTime, Money fee, Money discountAmount, DiscountCondition... discountConditions) {
+        super(title, runningTime, fee, discountConditions);
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    protected Money calculateDiscountAmount() {
+        return discountAmount;
+    }
+}
