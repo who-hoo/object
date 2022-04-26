2316700d4016b1231fcd25974433d84a69c1547e
who-hoo <who.ho3ov@gmail.com>
2022-04-27 00:16:38 +0900

    :sparkles: 전화 요금을 계산하는 애플리케이션 구현

---
+++ src/main/java/chapter10/Main.java
@@ -0,0 +1,22 @@
+package chapter10;
+
+import chapter02.Money;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Phone phone = new Phone(Money.wons(5), Duration.ofSeconds(10));
+        phone.call(new Call(
+            LocalDateTime.of(2022, 4, 27, 12, 10, 0),
+            LocalDateTime.of(2022, 4, 27, 12, 11, 0))
+        );
+        phone.call(new Call(
+            LocalDateTime.of(2022, 5, 27, 12, 10, 0),
+            LocalDateTime.of(2022, 5, 27, 12, 11, 0))
+        );
+
+        System.out.println(phone.calculateFee());
+    }
+}
