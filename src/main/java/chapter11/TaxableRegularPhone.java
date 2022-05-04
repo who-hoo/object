8620dda2edeece1eba4d7be8a234306dba0aac90
who-hoo <who.ho3ov@gmail.com>
2022-05-04 22:53:34 +0900

    :sparkles: 상속을 이용한 일반 요금제에 세금 정책 조합 구현

---
+++ src/main/java/chapter11/TaxableRegularPhone.java
@@ -0,0 +1,21 @@
+package chapter11;
+
+import chapter02.Money;
+import java.time.Duration;
+
+public class TaxableRegularPhone extends RegularPhone {
+
+    private double taxRate;
+
+    public TaxableRegularPhone(Money amount, Duration seconds,
+        double taxRate) {
+        super(amount, seconds);
+        this.taxRate = taxRate;
+    }
+
+    @Override
+    public Money calculateFee() {
+        Money fee = super.calculateFee();
+        return fee.plus(fee.times(taxRate));
+    }
+}
