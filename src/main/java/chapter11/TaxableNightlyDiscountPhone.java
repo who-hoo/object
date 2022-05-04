5dea86899a44ec7a0e9ed88145918a955ca223ad
who-hoo <who.ho3ov@gmail.com>
2022-05-04 23:10:18 +0900

    :sparkles: 상속을 이용한 심야 할인 요금제에 세금 정책 조합 구현

---
+++ src/main/java/chapter11/TaxableNightlyDiscountPhone.java
@@ -0,0 +1,20 @@
+package chapter11;
+
+import chapter02.Money;
+import java.time.Duration;
+
+public class TaxableNightlyDiscountPhone extends NightlyDiscountPhone {
+
+    private double taxRate;
+
+    public TaxableNightlyDiscountPhone(Money nightlyAmount, Money regularAmount, Duration seconds,
+        double taxRate) {
+        super(nightlyAmount, regularAmount, seconds);
+        this.taxRate = taxRate;
+    }
+
+    @Override
+    protected Money afterCalculated(Money fee) {
+        return fee.plus(fee.times(taxRate));
+    }
+}
