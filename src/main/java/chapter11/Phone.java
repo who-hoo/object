e2e7e653841c34be61bd6fc20c2e86ff968d0b7b
who-hoo <who.ho3ov@gmail.com>
2022-05-04 22:47:40 +0900

    :sparkles: 상속을 이용해서 기본 정책(일반 요금제, 심야 할인 요금제) 구현
    Phone 추상 클래스를 루트로 삼는 기존의 상속 계층을 이용하여 기본 정책 구현

---
+++ src/main/java/chapter11/Phone.java
@@ -0,0 +1,23 @@
+package chapter11;
+
+import chapter02.Money;
+import chapter10.Call;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Phone {
+
+    private List<Call> calls = new ArrayList<>();
+
+    public Money calculateFee() {
+        Money result = Money.ZERO;
+
+        for (Call call : calls) {
+            result = result.plus(calculateCallFee(call));
+        }
+
+        return result;
+    }
+
+    protected abstract Money calculateCallFee(Call call);
+}
---
+++ src/main/java/chapter11/RegularPhone.java
@@ -0,0 +1,21 @@
+package chapter11;
+
+import chapter02.Money;
+import chapter10.Call;
+import java.time.Duration;
+
+public class RegularPhone extends Phone {
+
+    private Money amount;
+    private Duration seconds;
+
+    public RegularPhone(Money amount, Duration seconds) {
+        this.amount = amount;
+        this.seconds = seconds;
+    }
+
+    @Override
+    protected Money calculateCallFee(Call call) {
+        return amount.times(call.getDuration().getSeconds() / seconds.getSeconds());
+    }
+}
