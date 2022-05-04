7c660564adbf65b7e044bcbe82b052ba0bfd5ff9
who-hoo <who.ho3ov@gmail.com>
2022-05-04 23:42:02 +0900

    :sparkles: 부가 정책 합성 관계로 변경

---
+++ src/main/java/chapter11/AdditionalRatePolicy.java
@@ -0,0 +1,20 @@
+package chapter11;
+
+import chapter02.Money;
+
+public abstract class AdditionalRatePolicy implements RatePolicy {
+
+    private RatePolicy next;
+
+    public AdditionalRatePolicy(RatePolicy next) {
+        this.next = next;
+    }
+
+    @Override
+    public Money calculateFee(Phone phone) {
+        Money fee = next.calculateFee(phone);
+        return afterCalculated(fee);
+    }
+
+    abstract protected Money afterCalculated(Money fee);
+}
